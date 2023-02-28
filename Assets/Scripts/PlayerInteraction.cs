c8e54c5cce0bb9685b9aea9e2b5e6cae5bd642b7
morrisseydylan <dylan.morrissey2002@gmail.com>
2023-02-28 13:40:17 -0500

    Player movement

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
     GameObject interaction;
+    bool move = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,17 +30,40 @@ public class PlayerInteraction : MonoBehaviour
                 }
                 interaction = hitObj;
                 SetOutline(true);
-                return;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    move = true;
+                }
+            }
+            else
+            {
+                SetOutline(false);
             }
         }
-        SetOutline(false);
+        else
+        {
+            SetOutline(false);
+        }
+        
+        if (move)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, interaction.transform.position, 0.005f);
+            if ((interaction.transform.position - transform.position).magnitude < 3.0f)
+            {
+                move = false;
+            }
+        }
     }
 
-    void SetOutline(bool outline)
+    void SetOutline(bool value)
     {
         if (interaction != null)
         {
-            interaction.GetComponent<Outline>().enabled = outline;
+            Outline outline;
+            if ((outline = interaction.GetComponent<Outline>()) != null)
+            {
+                outline.enabled = value;
+            }
         }
     }
 }
